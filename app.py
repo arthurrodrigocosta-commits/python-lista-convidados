b2329b0f5a9b33994381a484471da3e2fb84d469
arthurrodrigocosta-commits <arthur.rodrigo.costa@escola.pr.gov.br>
2025-08-25 09:40:22 -0300

    crie as lista

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
 # Lista de convidados que foram convidados para festa
+convidados-da-festa = ['nayellie','igor','gabriel,']
+
+dicionário vazio para armazenar  o status de presença de cada pessoa
+status_
